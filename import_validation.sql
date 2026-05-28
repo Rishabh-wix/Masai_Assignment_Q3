d1e4435abc09d63e2a97ba9eacf23a94301c2d9e
Rishabh-wix <rishabhsrivastava2004@gmail.com>
2026-05-28 22:12:37 +0530

    Create import_validation.sql

---
+++ import_validation.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(*) FROM students;
+SELECT COUNT(*) FROM courses;
+SELECT COUNT(*) FROM enrollments;
+
+SELECT COUNT(DISTINCT student_id) FROM students;
+SELECT COUNT(DISTINCT course_id) FROM courses;
